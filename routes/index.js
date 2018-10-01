f008f3770562d60b4d243464c335d155619ee8f3
Tom852 <thomaskistler@bluewin.ch>
2018-10-01 11:06:40 +0200

    erste schritte

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Overview' });
+});
+
+router.get('/task/', function(req, res, next) {
+    res.render('add_task', { title: 'NEw Task Add View' });
+});
+
+router.get('/task/:id/', function(req, res, next) {
+    res.render('task', { title: 'Detail View' });
 });
 
 module.exports = router;
